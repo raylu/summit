c4e51002d0e3d1eae8d01ed02f4f1708ed90005b
idunnololz <garyguo9@gmail.com>
2023-07-02 16:54:27 -0400

    Test release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.idunnololz.summit"
         minSdk = 21
         targetSdk = 33
-        versionCode = 18
-        versionName = "0.1.17"
+        versionCode = 19
+        versionName = "0.1.18"
 
         ksp {
             arg("room.schemaLocation", "$projectDir/schemas")
