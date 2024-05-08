9db5aade1c921dafe4f831f8641e42b23a256bea
Gary <garyguo9@gmail.com>
2024-05-07 23:45:20 -0400

    v1.36.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.idunnololz.summit"
         minSdk = 21
         targetSdk = 34
-        versionCode = 163
-        versionName = "1.35.1"
+        versionCode = 164
+        versionName = "1.36.0"
 
         ksp {
             arg("room.schemaLocation", "$projectDir/schemas")
--- app/src/main/java/com/idunnololz/summit/util/Changelog.kt
+++ app/src/main/java/com/idunnololz/summit/util/Changelog.kt
@@ -8,7 +8,7 @@ import com.idunnololz.summit.main.MainActivity
  */
 
 fun MainActivity.launchChangelog() {
-    launchPage(PostRef("lemmy.world", 14856560), switchToNativeInstance = true)
+    launchPage(PostRef("lemmy.world", 15149365), switchToNativeInstance = true)
 }
 fun BaseFragment<*>.launchChangelog() {
     getMainActivity()?.launchChangelog()
