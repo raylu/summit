f6490348e06f2ef3f605807fe3157a3229a94b19
Gary <garyguo9@gmail.com>
2025-03-21 22:20:14 -0400

    Remove google services plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
     id("androidx.navigation.safeargs.kotlin")
     id("org.jetbrains.kotlin.plugin.parcelize")
